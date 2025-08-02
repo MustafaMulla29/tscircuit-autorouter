cdae06ca9a20691eec1a245b4b4c96e68fdce181
Mustafa Mulla <97171641+MustafaMulla29@users.noreply.github.com>
2025-08-02 15:13:13 +0000

    Added a bug in the bug-reports dir

---
+++ examples/bug-reports/bugreport2-bc4361/bugreport2-bc4361.fixture.tsx
@@ -0,0 +1,7 @@
+// @ts-nocheck
+import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
+import bugReportJson from "./bugreport2-bc4361.json"
+
+export default () => {
+  return <AutoroutingPipelineDebugger srj={bugReportJson.simple_route_json} />;
+}
